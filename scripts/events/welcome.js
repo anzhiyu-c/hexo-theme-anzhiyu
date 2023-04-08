a484ba6968c363da673be9db58ff9d13ee645f51
anzhiyu <2268025923@qq.com>
2023-04-08 22:18:07 +0800

    修复log问题

--- scripts/events/welcome.js
+++ scripts/events/welcome.js
@@ -1,8 +1,6 @@
-const logger = require("hexo-log")();
-
 hexo.on("ready", () => {
   const { version } = require("../../package.json");
-  logger.info(`
+  hexo.log.info(`
   ===================================================================
                                                                      
    █████╗ ███╗   ██╗███████╗██╗  ██╗██╗██╗   ██╗██╗   ██╗
