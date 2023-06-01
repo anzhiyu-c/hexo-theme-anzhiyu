2420b92e3709dfcda63bfd2ccad16d79d2867f36
anzhiyu <2268025923@qq.com>
2023-06-01 17:26:33 +0800

    优化btns标签

--- scripts/tag/btns.js
+++ scripts/tag/btns.js
@@ -1,35 +1,35 @@
-'use strict';
+"use strict";
 
 function postBtns(args, content) {
-  return `<div class="btns ${args.join(' ')}">
+  return `<div class="btns ${args.join(" ")}">
             ${content}
           </div>`;
 }
 
 function postCell(args, content) {
-  args = args.join(' ').split(',')
-  let text = args[0] || ''
-  let url = args[1] || ''
-  text = text.trim()
-  url = url.trim()
+  args = args.join(" ").split(",");
+  let text = args[0] || "";
+  let url = args[1] || "";
+  text = text.trim();
+  url = url.trim();
   if (url.length > 0) {
-    url = "href='" + url + "'"
+    url = "href='" + url + "'";
   }
-  let icon = ''
-  let img = 'https://unpkg.zhimg.com/hexo-butterfly-tag-plugins-plus@latest/lib/assets/default.svg'
+  let icon = "";
+  let img = "https://npm.elemecdn.com/hexo-butterfly-tag-plugins-plus/lib/assets/default.svg";
   if (args.length > 2) {
-    if (args[2].indexOf(' fa-') > -1) {
-      icon = args[2].trim()
+    if (args[2].indexOf(" fa-") > -1) {
+      icon = args[2].trim();
     } else {
-      img = args[2].trim()
+      img = args[2].trim();
     }
   }
   if (icon.length > 0) {
-    return `<a class="button" ${url} title='${text}'><i class='${icon}'></i>${text}</a>`
+    return `<a class="button no-text-decoration" ${url} title='${text}'><i class='${icon}'></i>${text}</a>`;
   } else {
-    return `<a class="button" ${url} title='${text}'><img src='${img}'>${text}</a>`
+    return `<a class="button no-text-decoration" ${url} title='${text}'><img src='${img}'>${text}</a>`;
   }
 }
 
-hexo.extend.tag.register('btns', postBtns, {ends: true});
-hexo.extend.tag.register('cell', postCell);
+hexo.extend.tag.register("btns", postBtns, { ends: true });
+hexo.extend.tag.register("cell", postCell);
