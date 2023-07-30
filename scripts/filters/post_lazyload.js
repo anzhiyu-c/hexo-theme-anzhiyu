8d8ad12743e19a570584a6c068ca62c23e5e93a4
anzhiyu <73864136+anzhiyu-c@users.noreply.github.com>
2023-07-30 13:35:59 +0800

    新增文章内图片无法加载时加重error_img

--- scripts/filters/post_lazyload.js
+++ scripts/filters/post_lazyload.js
@@ -9,12 +9,13 @@
 const urlFor = require("hexo-util").url_for.bind(hexo);
 
 function lazyload(htmlContent) {
+  const error_img = hexo.theme.config.error_img.post_page
   const bg = hexo.theme.config.lazyload.placeholder
     ? urlFor(hexo.theme.config.lazyload.placeholder)
     : "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7";
   return htmlContent.replace(
     /(<img(?!.class[\t]*=[\t]*['"].*?nolazyload.*?['"]).*? src=)/gi,
-    `$1 "${bg}" data-lazy-src=`
+    `$1 "${bg}" onerror="this.onerror=null,this.src=&quot;${error_img}&quot;" data-lazy-src=`
   );
 }
 
