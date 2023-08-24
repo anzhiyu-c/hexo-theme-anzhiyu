833eb0be0e6a1da35a09e8dffb81dfbba8b78803
anzhiyu <73864136+anzhiyu-c@users.noreply.github.com>
2023-08-24 12:05:37 +0800

    变量提升，pjax刷新时重新获取dom元素，解决控制台等地方pjax刷新问题

--- source/js/utils.js
+++ source/js/utils.js
@@ -645,7 +645,7 @@ const anzhiyu = {
     const evt = new Event("input", { cancelable: true, bubbles: true });
     const defaultPlaceholder =
       "昵称（请勿包含博客等字样）：\n网站地址（要求博客地址，请勿提交个人主页）：\n头像图片url（请提供尽可能清晰的图片，我会上传到我自己的图床）：\n描述：\n站点截图（可选）：\n";
-    input.value = this.getConfigIfPresent(GLOBAL_CONFIG.linkPageTop, 'addFriendPlaceholder', defaultPlaceholder)
+    input.value = this.getConfigIfPresent(GLOBAL_CONFIG.linkPageTop, "addFriendPlaceholder", defaultPlaceholder);
     input.dispatchEvent(evt);
     input.focus();
     input.setSelectionRange(-1, -1);
