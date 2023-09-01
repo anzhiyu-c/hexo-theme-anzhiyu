b2fc264a09c7515391590ed7abdf562ca570b9bf
anzhiyu-c <me@anheyu.com>
2023-09-01 15:34:04 +0800

    支持anheyu CDN pace样式升级

--- scripts/events/cdn.js
+++ scripts/events/cdn.js
@@ -93,6 +93,7 @@ hexo.extend.filter.register("before_generate", () => {
         elemecdn: `https://npm.elemecdn.com/${name}${verType}/${file}`,
         onmicrosoft: `https://npm.onmicrosoft.cn/${name}${verType}/${file}`,
         cbd: `https://cdn.cbd.int/${name}${verType}/${file}`,
+        anheyu: `https://cdn.anheyu.com/npm/${name}${verType}/${min_file}`,
         custom: (CDN.custom_format || "").replace(/\$\{(.+?)\}/g, (match, $1) => value[$1]),
       };
 
