ee3ceb1c32882149a3a9a4c8f077a06aa3931214
Kayro <kayro2007@qq.com>
2026-02-20 17:06:25 +0800

    chore(theme): customize nav, clock key, console log and music playlist

--- source/js/utils.js
+++ source/js/utils.js
@@ -845,7 +845,7 @@ const anzhiyu = {
       return;
     }
     const urlParams = new URLSearchParams(window.location.search);
-    const userId = "8152976493";
+    const userId = "12607574027";
     const userServer = "netease";
     const anMusicPageMeting = document.getElementById("anMusic-page-meting");
     if (urlParams.get("id") && urlParams.get("server")) {
